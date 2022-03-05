b4265084ada1645cb7de8f0a858ce8e2e322648c
prashant-vaidwan038 <58844559+prashant-vaidwan038@users.noreply.github.com>
2022-03-05 07:34:55 +0530

    AssertEquals

---
+++ src/TestAssert.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAssert {
+
+	@Test
+	public void simple()
+	{
+		int A = 27;
+		int B = 27;
+		assertEquals(A,B);
+		
+	}
+@Test
+    public void simple1()
+   {
+	
+int A = 4;
+int B = 5;
+assertEquals(A,B);
+}
+     
+ 
+ @Test
+ public void simple2()
+ {
+	 int A = 8;
+	 int B = 9;
+	 assertEquals(A,B);
+ }
+     
+}
